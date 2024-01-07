623baf59198e37f4778856006b077b5a6c92d0f0
Bartosz Kozielski <bartoszkozielski1998@gmail.com>
2024-01-07 13:19:46 +0100

    commit

---
+++ docs/03-promises.b10f5643.js
@@ -0,0 +1,157 @@
+// modules are defined as an array
+// [ module function, map of requires ]
+//
+// map of requires is short require name -> numeric require
+//
+// anything defined in a previous bundle is accessed via the
+// orig method which is the require for previous bundles
+parcelRequire = (function (modules, cache, entry, globalName) {
+  // Save the require from previous bundle to this closure if any
+  var previousRequire = typeof parcelRequire === 'function' && parcelRequire;
+  var nodeRequire = typeof require === 'function' && require;
+
+  function newRequire(name, jumped) {
+    if (!cache[name]) {
+      if (!modules[name]) {
+        // if we cannot find the module within our internal map or
+        // cache jump to the current global require ie. the last bundle
+        // that was added to the page.
+        var currentRequire = typeof parcelRequire === 'function' && parcelRequire;
+        if (!jumped && currentRequire) {
+          return currentRequire(name, true);
+        }
+
+        // If there are other bundles on this page the require from the
+        // previous one is saved to 'previousRequire'. Repeat this as
+        // many times as there are bundles until the module is found or
+        // we exhaust the require chain.
+        if (previousRequire) {
+          return previousRequire(name, true);
+        }
+
+        // Try the node require function if it exists.
+        if (nodeRequire && typeof name === 'string') {
+          return nodeRequire(name);
+        }
+
+        var err = new Error('Cannot find module \'' + name + '\'');
+        err.code = 'MODULE_NOT_FOUND';
+        throw err;
+      }
+
+      localRequire.resolve = resolve;
+      localRequire.cache = {};
+
+      var module = cache[name] = new newRequire.Module(name);
+
+      modules[name][0].call(module.exports, localRequire, module, module.exports, this);
+    }
+
+    return cache[name].exports;
+
+    function localRequire(x){
+      return newRequire(localRequire.resolve(x));
+    }
+
+    function resolve(x){
+      return modules[name][1][x] || x;
+    }
+  }
+
+  function Module(moduleName) {
+    this.id = moduleName;
+    this.bundle = newRequire;
+    this.exports = {};
+  }
+
+  newRequire.isParcelRequire = true;
+  newRequire.Module = Module;
+  newRequire.modules = modules;
+  newRequire.cache = cache;
+  newRequire.parent = previousRequire;
+  newRequire.register = function (id, exports) {
+    modules[id] = [function (require, module) {
+      module.exports = exports;
+    }, {}];
+  };
+
+  var error;
+  for (var i = 0; i < entry.length; i++) {
+    try {
+      newRequire(entry[i]);
+    } catch (e) {
+      // Save first error but execute all entries
+      if (!error) {
+        error = e;
+      }
+    }
+  }
+
+  if (entry.length) {
+    // Expose entry point to Node, AMD or browser globals
+    // Based on https://github.com/ForbesLindesay/umd/blob/master/template.js
+    var mainExports = newRequire(entry[entry.length - 1]);
+
+    // CommonJS
+    if (typeof exports === "object" && typeof module !== "undefined") {
+      module.exports = mainExports;
+
+    // RequireJS
+    } else if (typeof define === "function" && define.amd) {
+     define(function () {
+       return mainExports;
+     });
+
+    // <script>
+    } else if (globalName) {
+      this[globalName] = mainExports;
+    }
+  }
+
+  // Override the current require with this new one
+  parcelRequire = newRequire;
+
+  if (error) {
+    // throw error from earlier, _after updating parcelRequire_
+    throw error;
+  }
+
+  return newRequire;
+})({"GyVV":[function(require,module,exports) {
+function createPromise(position, delay) {
+  return new Promise(function (resolve, reject) {
+    var shouldResolve = Math.random() > 0.3;
+    setTimeout(function () {
+      if (shouldResolve) {
+        resolve({
+          position: position,
+          delay: delay
+        });
+      } else {
+        reject({
+          position: position,
+          delay: delay
+        });
+      }
+    }, delay);
+  });
+}
+document.querySelector('.form').addEventListener('submit', function (event) {
+  event.preventDefault();
+  var delay = parseInt(document.querySelector('input[name="delay"]').value, 10);
+  var step = parseInt(document.querySelector('input[name="step"]').value, 10);
+  var amount = parseInt(document.querySelector('input[name="amount"]').value, 10);
+  for (var i = 1; i <= amount; i++) {
+    createPromise(i, delay + (i - 1) * step).then(function (_ref) {
+      var position = _ref.position,
+        delay = _ref.delay;
+      console.log("\u2705 Fulfilled promise ".concat(position, " in ").concat(delay, "ms"));
+    }).catch(function (_ref2) {
+      var position = _ref2.position,
+        delay = _ref2.delay;
+      console.log("\u274C Rejected promise ".concat(position, " in ").concat(delay, "ms"));
+    });
+  }
+});
+},{}]},{},["GyVV"], null)
+//# sourceMappingURL=/goit-js-hw-09/03-promises.b10f5643.js.map
